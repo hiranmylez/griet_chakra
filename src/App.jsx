23cf1f2544a3faa742c02fb09319766bc564a72e
hiranmylez <hiranmayeed07@gmail.com>
2025-09-24 22:37:03 +0530

    Resolve merge conflicts and add auth project

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
-import NiramayaAuthSystem from './pages/AuthPage';
-
-function App() {
-  return (
-    <NiramayaAuthSystem />
-=======
 import React from 'react';
 import NiramayaLanding from './NiramayaLanding';
 
 function App() {
   return (
     <NiramayaLanding />
->>>>>>> 9e0be274d5ef06213a01bc0833ecc657c174d352
   );
 }
 
